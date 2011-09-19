c1f437392348d718ae51db2307b82d2569b08f5b
speg03 <speg03@gmail.com>
2011-09-19 15:39:40 +0900

    受信、送信後の画面を別にした。

--- src/com/appspot/speg03/btmessenger/ReceiveMessageActivity.java
+++ src/com/appspot/speg03/btmessenger/ReceiveMessageActivity.java
@@ -20,7 +20,7 @@ public class ReceiveMessageActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.action);
 
         mAdapter = BluetoothAdapter.getDefaultAdapter();
         try {
--- src/com/appspot/speg03/btmessenger/SendMessageActivity.java
+++ src/com/appspot/speg03/btmessenger/SendMessageActivity.java
@@ -19,7 +19,7 @@ public class SendMessageActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.action);
 
         String senderAddress = getResources().getString(R.string.bluetooth_dest_address);
         mAdapter = BluetoothAdapter.getDefaultAdapter();
